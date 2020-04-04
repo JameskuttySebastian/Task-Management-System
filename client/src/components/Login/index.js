7ca3c5fbfc204911e3210a96b823c642fcb85062
Anuradha Dambaladeniya <anuradhadambaladeniya@Anuradhas-MBP.modem>
2020-04-04 18:02:36 +1030

    adding login page

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -16,11 +16,11 @@ function Login() {
             <div className="login-form">
                <form>
                   <div className="form-group">
-                     <label>User Name</label>
+                     {/* <label>User Name</label> */}
                      <input type="text" className="form-control" placeholder="User Name"></input>
                   </div>
                   <div className="form-group">
-                     <label>Password</label>
+                     {/* <label>Password</label> */}
                      <input type="password" className="form-control" placeholder="Password"></input>
                   </div>
                   <button type="submit" className="btn btn-black">Login</button>
