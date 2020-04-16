7d17a4d62a67ac8f72d81693679cb6cd6f5b1e4b
Jameskutty Sebastian <jameskutty.sebastian@gmail.com>
2020-04-17 01:09:41 +0930

    added models, controllers and routes : basic

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,15 @@
 const router = require("express").Router();
-const bookRoutes = require("./books");
+// const bookRoutes = require("./books");
+const client_taskRoutes = require("./client_task");
+const clientRoutes = require("./client");
+const taskRoutes = require("./task");
+const userRoutes = require("./user");
 
 // Book routes
-router.use("/books", bookRoutes);
+// router.use("/books", bookRoutes);
+router.use("/api/client_task", client_taskRoutes);
+router.use("/api/client", clientRoutes);
+router.use("/api/task", taskRoutes);
+router.use("/api/user", userRoutes);
 
 module.exports = router;
