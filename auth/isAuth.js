390f4562df4c8527289f2189c2f03a5488d403df
Jameskutty Sebastian <jameskutty.sebastian@gmail.com>
2020-04-19 21:00:27 +0930

    solved return error for secured route

--- auth/isAuth.js
+++ auth/isAuth.js
@@ -1,9 +1,9 @@
 const { verify } = require("jsonwebtoken");
 
-const isAuth = async (req) => {
+const isAuth = async (req, res) => {
   try {
     const authorization = req.headers["authorization"];
-    console.log("isAuth : authorization : " + authorization);
+    // console.log("isAuth : authorization : " + authorization);
     if (!authorization) throw new Error("Please login again");
     const token = authorization.split(" ")[1];
     const { userId } = await verify(token, process.env.ACCESS_TOKEN_SECRET);
@@ -11,11 +11,12 @@ const isAuth = async (req) => {
     if (userId == "undefined" || userId == "") {
       throw new Error("Please login again 3");
     } else {
-      console.log("isAuth : userId : " + userId);
+      // console.log("isAuth : userId : " + userId);
       return userId;
     }
   } catch (err) {
-    console.log("Auth error : " + err.message);
+    // console.log("Auth error : " + err.message);
+    res.status(401).json({ message: err.message + ". Please login again" });
   }
 };
 
--- controllers/registerController.js
+++ controllers/registerController.js
@@ -15,12 +15,16 @@ const { isAuth } = require("../auth/isAuth");
 // {
 //     "message": "User created"
 // }
+// Duplicate insersion will show following message
+// {
+//   "message": "Validation error"
+// }
 module.exports = {
   create: async function (req, res) {
     try {
       // checking the user is already logged in
-      const userId = await isAuth(req);
-      console.log("registerController : userId : " + userId);
+      const userId = await isAuth(req, res);
+      // console.log("registerController : userId : " + userId);
 
       //1. check if user exists
       // This part is omitted because sequelize will do this part using primary key
@@ -30,21 +34,25 @@ module.exports = {
         //2.  if user not exist, hash the password
         const { password } = req.body;
         const hashedPassword = hashSync(password, 10);
-        console.log("registerController : hashedPassword : " + hashedPassword);
+        // console.log("registerController : hashedPassword : " + hashedPassword);
         //3. insert the user
         const result = await db.User.create({
           password: hashedPassword,
           ...req.body,
+        }).then((result) => {
+          // console.log(result);
         });
-        console.log("registerController : result : " + result);
+        // console.log("registerController : result : " + result);
         //   res.send(JSON.stringify(result))
         //   res.json(result);
-        res.send({ message: "User created" });
+        // res.send({ message: "User created" });
+        res.status(200).json({ message: "User created" });
       } else {
         throw new Error("Please login again");
       }
     } catch (err) {
-      console.log("registerController : err : " + err);
+      // console.log("Auth error : " + err.message);
+      res.status(401).json({ message: err.message });
     }
   },
 };
