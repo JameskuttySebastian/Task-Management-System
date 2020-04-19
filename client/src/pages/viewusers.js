307589aa6e809b20701b10c98ac33730766d886a
anu-dam <anuradha.dambaladeniya@gmail.com>
2020-04-19 15:42:44 +0930

    bootstrap issue

--- client/src/pages/viewusers.js
+++ client/src/pages/viewusers.js
@@ -10,52 +10,34 @@ import TablePagination from '@material-ui/core/TablePagination';
 import TableRow from '@material-ui/core/TableRow';
 
 const columns = [
-  { id: 'name', label: 'Name', minWidth: 170 },
-  { id: 'code', label: 'ISO\u00a0Code', minWidth: 100 },
+  { id: 'name', label: 'Name', minWidth: 250 },
+  { id: 'email', label: 'Email ID', minWidth: 300 },
   {
-    id: 'population',
-    label: 'Population',
+    id: 'type',
+    label: 'User Type',
+    minWidth: 170,
+    // align: 'right',
+    format: (value) => value.toLocaleString(),
+  },
+  {
+    id: 'status',
+    label: 'Status',
     minWidth: 170,
     align: 'right',
     format: (value) => value.toLocaleString(),
   },
-  {
-    id: 'size',
-    label: 'Size\u00a0(km\u00b2)',
-    minWidth: 170,
-    align: 'right',
-    format: (value) => value.toLocaleString(),
-  },
-  {
-    id: 'density',
-    label: 'Density',
-    minWidth: 170,
-    align: 'right',
-    format: (value) => value.toFixed(2),
-  },
+ 
 ];
 
-function createData(name, code, population, size) {
-  const density = population / size;
-  return { name, code, population, size, density };
+function createData(name, email, type, status) {
+  
+  return { name, email, type, status };
 }
 
 const rows = [
-  createData('India', 'IN', 1324171354, 3287263),
-  createData('China', 'CN', 1403500365, 9596961),
-  createData('Italy', 'IT', 60483973, 301340),
-  createData('United States', 'US', 327167434, 9833520),
-  createData('Canada', 'CA', 37602103, 9984670),
-  createData('Australia', 'AU', 25475400, 7692024),
-  createData('Germany', 'DE', 83019200, 357578),
-  createData('Ireland', 'IE', 4857000, 70273),
-  createData('Mexico', 'MX', 126577691, 1972550),
-  createData('Japan', 'JP', 126317000, 377973),
-  createData('France', 'FR', 67022000, 640679),
-  createData('United Kingdom', 'GB', 67545757, 242495),
-  createData('Russia', 'RU', 146793744, 17098246),
-  createData('Nigeria', 'NG', 200962417, 923768),
-  createData('Brazil', 'BR', 210147125, 8515767),
+  createData('James', 'james@mail.com', 'Administrator', 'Active'),
+  createData('Chandu', 'Chandu@mail.com', 'Administrator', 'Active'),
+
 ];
 
 const useStyles = makeStyles({
@@ -94,7 +76,7 @@ export default function StickyHeadTable() {
                 <TableCell
                   key={column.id}
                   align={column.align}
-                  style={{ minWidth: column.minWidth }}
+                  style={{ minWidth: column.minWidth, fontWeight: 'bold' }}
                 >
                   {column.label}
                 </TableCell>
