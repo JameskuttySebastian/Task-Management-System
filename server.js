bbcd8ec5736b39b1f56a5ec3c9b910a7475d5c64
anu-dam <anuradha.dambaladeniya@gmail.com>
2020-04-23 00:11:52 +0930

    changing config.json file

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use(routes);
 app.use(express.urlencoded({ extended: true })); // support url encoded bodies
 
 // Start the API server
-db.sequelize.sync({ force: false }).then(function () {
+db.sequelize.sync({ force: true }).then(function () {
   app.listen(PORT, function () {
     console.log(
       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
