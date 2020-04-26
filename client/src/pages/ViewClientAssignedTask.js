db39766e3d31330fa6a9ee5e39dd601217ce83d0
Jameskutty Sebastian <jameskutty.sebastian@gmail.com>
2020-04-26 19:56:17 +0930

    added files for client views

---
+++ client/src/pages/ViewClientAssignedTask.js
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from "react";
+import MaterialTable from "material-table";
+import API from "../utils/API/API";
+import { useHistory } from "react-router-dom";
+import AssignmentOutlinedIcon from "@material-ui/icons/AssignmentOutlined";
+
+export default function ViewAssignedTask() {
+  const [assignedTaskData, setAssignedTaskData] = useState([]);
+  const [selectedRow, setSelectedRow] = useState(null);
+  const history = useHistory();
+
+  useEffect(() => {
+    API.apiGetAssignedTask()
+      .then((response) => {
+        setAssignedTaskData(response.data);
+        // console.log(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  const viewAssignedTaskDetail = (e, rowData) => {
+    history.push(`/viewAssignedTaskDetail/${rowData.clienttasks_id}`);
+  };
+
+  return (
+    <div style={{ clear: "both", marginTop: 60 }}>
+      <MaterialTable
+        title="All Assigned Tasks"
+        columns={[
+          {
+            title: "ID No",
+            field: "clienttasks_id",
+            type: "numeric",
+            width: "10%",
+            textAlign: "center",
+          },
+          { title: "Client", field: "clients_name" },
+          { title: "Title", field: "tasks_title" },
+          { title: "Details", field: "tasks_description" },
+          { title: "Completed by", field: "tasks_completedBy" },
+          { title: "Status", field: "clienttasks_status" },
+        ]}
+        data={assignedTaskData}
+        onRowClick={async (evt, selectedRows) =>
+          await setSelectedRow(selectedRows)
+        }
+        options={{
+          actionsColumnIndex: -1,
+          sorting: true,
+          headerStyle: {
+            backgroundColor: "#01579b",
+            color: "#FFF",
+          },
+          rowStyle: (rowData) => ({
+            backgroundColor:
+              selectedRow && selectedRow.tableData.id === rowData.tableData.id
+                ? "#EEE"
+                : "#FFF",
+          }),
+        }}
+        actions={[
+          (rowData) => ({
+            icon: AssignmentOutlinedIcon,
+            tooltip: "View Details",
+            onClick: (event, rowData) => viewAssignedTaskDetail(event, rowData),
+          }),
+        ]}
+      />
+    </div>
+  );
+}
